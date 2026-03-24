ee7a4883456b95f3a03514eb4cf73f419f06e660
Md salim islam <salimislam0036@gmail.com>
2026-03-24 09:59:16 +0600

    chore: remove unnecessary comments

--- src/api/categoryApi.js
+++ src/api/categoryApi.js
@@ -1,8 +1,7 @@
 import axiosInstance from './axiosInstance';
-// categoryApi.js
-export const getAllCategories = async (page = 1, limit = 6) => { // এখানে limit যোগ করলাম
+
+export const getAllCategories = async (page = 1, limit = 6) => {
   try {
-    // URL-এ limit প্যারামিটারটি পাঠিয়ে দাও
     const response = await axiosInstance.get(`/category?page=${page}&limit=${limit}`);
     return response.data;
   } catch (error) {
@@ -10,20 +9,11 @@ export const getAllCategories = async (page = 1, limit = 6) => { // এখান
   }
 };
 
-// export const getAllCategories = async (page = 1) => {
-//   try {
- 
-//     const response = await axiosInstance.get(`/category?page=${page}`);
-//     return response.data;
-//   } catch (error) {
-//     throw error.response ? error.response.data : new Error("Network Error");
-//   }
-// };
+
 
 export const getCategoryById = async (id) => {
   try {
     const response = await axiosInstance.get(`/category/${id}`);
-    // আপনার ব্যাকএন্ড যেহেতু { success: true, data: category } পাঠাচ্ছে
     return response.data; 
   } catch (error) {
     throw error.response ? error.response.data : new Error("Network Error");
